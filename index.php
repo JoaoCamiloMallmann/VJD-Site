0a5e7b714ea56a7973efbf60e80213188f9b67d3
João Camilo M <jcamilomallmann@hotmail.com>
2023-03-12 17:17:23 -0300

    .

--- index.php
+++ index.php
@@ -6,15 +6,17 @@ require('header.php');
 
   <section class="secao-sobre-nos secao-hide">
 
-    <article class="textoSobreNos">
+    <article class="textoSobreNos ">
 
       <h3>A <span> Quebrando Padrões</span> é uma empresa de prestação de serviços do ramo de design que busca melhorar a cada dia o atendimento personalizado ao cliente.
       </h3>
 
       <p>Aqui você encontrará soluções criativas e inovadoras para todas as suas necessidades de design. Seja para criação de logotipos, identidade visual, layouts para mídias sociais, criação e design para sites ou qualquer outro projeto. Nosso time acredita que o design é mais do que apenas estética, é uma ferramenta poderosa para comunicar sua marca e se conectar com seu público-alvo.
       </p>
-      
-      <img class="sobreNosSeta" src="<?php echo DIR_WS_SISTEMA . 'inc/img/Seta.png' ?>" alt="Seta">
+
+      <div class="sobreNosSeta">
+        <img src="<?php echo DIR_WS_SISTEMA . 'inc/img/Seta.png' ?>" alt="Seta">
+      </div>
 
     </article>
 
@@ -22,8 +24,8 @@ require('header.php');
 
   <section class="secao-logo">
 
-    <img class="LogoPrincipal" src="<?php echo( DIR_WS_SISTEMA_INC . 'img/logoEmpresa.png' ) ?>" alt="Logo">
-    
+    <img class="LogoPrincipal" src="<?php echo (DIR_WS_SISTEMA_INC . 'img/logoEmpresa.png') ?>" alt="Logo">
+
     <div class="textoHome">
       <p>Transformando</p>
       <p>ideias em</p>
